7c4b3944a47c06f609151fc1478fafc24370a2e1
Christian Braybrooke <chris@purplemountmedia.com>
2018-10-01 16:45:00 +0100

    Spelling

--- config/ecommerce.php
+++ config/ecommerce.php
@@ -3,19 +3,20 @@
 /**
  * Your package config would go here
  */
-$green = '#93d250';
-$red = '#fe0002';
-$blue = '#66cbff';
+
+ $green = '#93d250';
+ $red = '#fe0002';
+ $blue = '#66cbff';
 
 return [
 
   'web_uri' => env('ECOMMERCE_WEB_URI', 'admin'),
 
-
   'web_middleware' => env('ECOMMERCE_WEB_MIDDLEWARE', 'web'),
 
-
   'api_uri' => env('ECOMMERCE_API_URI', 'api/ecommerce'),
+
+  'theme_color' => env('THEME_COLOR', '#409eff'),
 
   'models' => [
 
@@ -38,7 +39,7 @@ return [
       'media' => ChrisBraybrooke\ECommerce\Models\Media::class,
 
       //
-      'order' => App\Order::class,
+      'order' => ChrisBraybrooke\ECommerce\Models\Order::class,
 
       //
       'page' => ChrisBraybrooke\ECommerce\Models\Page::class,
@@ -78,207 +79,234 @@ return [
 
       //
       'payment' => ChrisBraybrooke\ECommerce\Models\Payment::class,
+
+      //
+      'price' => ChrisBraybrooke\ECommerce\Models\Price::class,
   ],
 
   'shop_data' => [
-
-      //
-      'collection_mappings' => ['Home Header Collection', 'Home Panels Collection', 'Product Categories Collection'],
-
-      //
-      'site_images' => ['Website Logo', 'Website Logo Inverse'],
-
-      //
-      'aditional_cols' => [
-          'orders' => [
-              'Job Number' => [
-                  'prop' => 'ref_number',
-                  'sortable' => true,
-                  'label' => 'Job No.',
-                  'align' => 'left',
-                  'resizable' => true,
-                  'type' => 'button',
-                  'filled_background' => $green,
-                  'value' => 'content.data.$[content_name=In Production].content.date',
-                  'width' => '130px',
-                  'button' => [
-                      'type' => 'success',
-                      'plain' => true,
-                      'size' => 'mini',
-                      'text' => 'In Production',
-                      'only_show_hover' => true,
-                      'show_value' => true,
-                      'value' => 'ref_number',
-                      'hide_if_value' => true
-                  ],
-                  'action' => [
-                      'type' => 'api',
-                      'set' => 'content.data.$[content_name=In Production].content.date',
-                      'value' => now()->format('d-m-Y')
-                  ],
-                  'api' => [
-                      'method' => 'put',
-                      'path' => 'orders/${id}'
-                  ]
-              ],
-              'Specs' => [
-                  'prop' => 'specs_completed',
-                  'sortable' => true,
-                  'label' => 'Specs',
-                  'align' => 'left',
-                  'resizable' => true,
-                  'type' => 'button',
-                  'empty_background' => $red,
-                  'filled_background' => $green,
-                  'width' => '100px',
-                  'value' => 'content.data.$[content_name=Spec Completed].content.date',
-                  'button' => [
-                      'type' => 'danger',
-                      'plain' => true,
-                      'size' => 'mini',
-                      'text' => 'Complete',
-                      'show_value' => true,
-                      'value' => 'content.data.$[content_name=Spec Completed].content.date',
-                      'hide_if_value' => true
-                  ],
-                  'action' => [
-                      'type' => 'api',
-                      'set' => 'content.data.$[content_name=Spec Completed].content.date',
-                      'value' => now()->format('d-m-Y')
-                  ],
-                  'api' => [
-                      'method' => 'put',
-                      'path' => 'orders/${id}'
-                  ]
-              ],
-              'Materials' => [
-                  'prop' => 'materials_ordered',
-                  'sortable' => true,
-                  'label' => 'Materials',
-                  'align' => 'left',
-                  'resizable' => true,
-                  'type' => 'button',
-                  'empty_background' => $red,
-                  'filled_background' => $green,
-                  'width' => '100px',
-                  'value' => 'content.data.$[content_name=Materials Ordered].content.date',
-                  'button' => [
-                      'type' => 'danger',
-                      'plain' => true,
-                      'size' => 'mini',
-                      'text' => 'Ordered',
-                      'show_value' => true,
-                      'value' => 'content.data.$[content_name=Materials Ordered].content.date',
-                      'hide_if_value' => true
-                  ],
-                  'action' => [
-                      'type' => 'api',
-                      'set' => 'content.data.$[content_name=Materials Ordered].content.date',
-                      'value' => now()->format('d-m-Y')
-                  ],
-                  'api' => [
-                      'method' => 'put',
-                      'path' => 'orders/${id}'
-                  ]
-              ],
-              'Delivery Cost' => [
-                  'prop' => 'shipping_cost',
-                  'sortable' => true,
-                  'label' => 'Delivery Cost',
-                  'align' => 'left',
-                  'resizable' => true,
-                  'type' => 'addition',
-                  'empty_background' => $red,
-                  'filled_background' => $green,
-                  'width' => '120px',
-                  'value' => 'content.data.$[content_name=Delivery Information].content.Totals Multi',
-                  'addition' => [
-                      'show_total' => false,
-                      'total_text' => 'Deliveries',
-                      'total_button' => true,
-                      'format_amount' => true,
-                      'amount_prefix' => '£',
-                      'add_button_text' => 'Add Delivery',
-                      'individual_name' => 'Delivery',
-                      'form_name' => 'Add Delivery',
-                      'additional_fields' => ['Date' => '', 'Notes' => ''],
-                  ],
-                  'action' => [
-                      'type' => 'api',
-                      'set' => 'content.data.$[content_name=Delivery Information].content.Totals Multi',
-                  ],
-                  'api' => [
-                      'method' => 'put',
-                      'path' => 'orders/${id}'
-                  ]
-              ],
-              'Delivery Invoice' => [
-                  'prop' => 'shipping_invoice',
-                  'sortable' => true,
-                  'label' => 'Delivery Invoice',
-                  'align' => 'left',
-                  'resizable' => true,
-                  'type' => 'button',
-                  'empty_background' => $red,
-                  'filled_background' => $green,
-                  'width' => '130px',
-                  'value' => 'content.data.$[content_name=Delivery Information].content.invoice',
-                  'button' => [
-                      'type' => 'danger',
-                      'plain' => true,
-                      'size' => 'mini',
-                      'text' => 'Recieved',
-                      'show_value' => true,
-                      'value' => 'content.data.$[content_name=Delivery Information].content.invoice',
-                      'hide_if_value' => true
-                  ],
-                  'action' => [
-                      'type' => 'api',
-                      'set' => 'content.data.$[content_name=Delivery Information].content.invoice',
-                      'value' => now()->format('d-m-Y')
-                  ],
-                  'api' => [
-                      'method' => 'put',
-                      'path' => 'orders/${id}'
-                  ]
-              ],
+    //
+    'cart_url' => env('APP_URL') . '/cart',
+    //
+    'collection_mappings' => ['Home Header Collection', 'Home Panels Collection', 'Product Categories Collection', 'Advert Collection', 'Home Featured Product Collection'],
+    //
+    'site_images' => ['Website Logo', 'Website Logo Inverse', 'Product Video Placeholder', 'Brochure File'],
+    //
+    'aditional_cols' => [
+        'orders' => [
+            'Job Number' => [
+                'prop' => 'ref_number',
+                'sortable' => true,
+                'label' => 'Job No.',
+                'align' => 'left',
+                'resizable' => true,
+                'type' => 'button',
+                'filled_background' => $green,
+                'width' => '130px',
+                'value' => 'content.data.$[content_name=In Production].content.date',
+                'button' => [
+                    'type' => 'success',
+                    'plain' => true,
+                    'size' => 'mini',
+                    'text' => 'In Production',
+                    'only_show_hover' => true,
+                    'show_value' => true,
+                    'value' => 'ref_number',
+                    'hide_if_value' => true
+                ],
+                'action' => [
+                    'type' => 'api',
+                    'set' => 'content.data.$[content_name=In Production].content.date',
+                    'value' => now()->format('d-m-Y')
+                ],
+                'api' => [
+                    'method' => 'put',
+                    'path' => 'orders/${id}'
+                ]
+            ],
+            'Specs' => [
+                'prop' => 'specs_completed',
+                'sortable' => true,
+                'label' => 'Specs',
+                'align' => 'left',
+                'resizable' => true,
+                'type' => 'button',
+                'empty_background' => $red,
+                'filled_background' => $green,
+                'width' => '100px',
+                'value' => 'content.data.$[content_name=Spec Completed].content.date',
+                'button' => [
+                    'type' => 'danger',
+                    'plain' => true,
+                    'size' => 'mini',
+                    'text' => 'Complete',
+                    'show_value' => true,
+                    'value' => 'content.data.$[content_name=Spec Completed].content.date',
+                    'hide_if_value' => true
+                ],
+                'action' => [
+                    'type' => 'api',
+                    'set' => 'content.data.$[content_name=Spec Completed].content.date',
+                    'value' => now()->format('d-m-Y')
+                ],
+                'api' => [
+                    'method' => 'put',
+                    'path' => 'orders/${id}'
+                ]
+            ],
+            'Materials' => [
+                'prop' => 'materials_ordered',
+                'sortable' => true,
+                'label' => 'Materials',
+                'align' => 'left',
+                'resizable' => true,
+                'type' => 'button',
+                'empty_background' => $red,
+                'filled_background' => $green,
+                'width' => '100px',
+                'value' => 'content.data.$[content_name=Materials Ordered].content.date',
+                'button' => [
+                    'type' => 'danger',
+                    'plain' => true,
+                    'size' => 'mini',
+                    'text' => 'Ordered',
+                    'show_value' => true,
+                    'value' => 'content.data.$[content_name=Materials Ordered].content.date',
+                    'hide_if_value' => true
+                ],
+                'action' => [
+                    'type' => 'api',
+                    'set' => 'content.data.$[content_name=Materials Ordered].content.date',
+                    'value' => now()->format('d-m-Y')
+                ],
+                'api' => [
+                    'method' => 'put',
+                    'path' => 'orders/${id}'
+                ]
+            ],
+            // 'Delivery' => [
+            //     'prop' => 'Delivery_completed',
+            //     'sortable' => true,
+            //     'label' => 'Delivery',
+            //     'align' => 'left',
+            //     'resizable' => true,
+            //     'type' => 'button',
+            //     'empty_background' => $red,
+            //     'filled_background' => $green,
+            //     'width' => '100px',
+            //     'value' => 'content.data.$[content_name=Delivery Information].content.date',
+            //     'button' => [
+            //         'type' => 'danger',
+            //         'plain' => true,
+            //         'size' => 'mini',
+            //         'text' => 'Complete',
+            //         'show_value' => true,
+            //         'value' => 'content.data.$[content_name=Delivery Information].content.date',
+            //         'hide_if_value' => true
+            //     ],
+            //     'action' => [
+            //         'type' => 'api',
+            //         'set' => 'content.data.$[content_name=Delivery Information].content.date',
+            //         'value' => now()->format('d-m-Y')
+            //     ],
+            //     'api' => [
+            //         'method' => 'put',
+            //         'path' => 'orders/${id}'
+            //     ]
+            // ],
+            'Delivery Cost' => [
+                'prop' => 'shipping_cost',
+                'sortable' => true,
+                'label' => 'Delivery Cost',
+                'align' => 'left',
+                'resizable' => true,
+                'type' => 'addition',
+                'empty_background' => $red,
+                'filled_background' => $green,
+                'width' => '120px',
+                'value' => 'content.data.$[content_name=Delivery Information].content.Totals Multi',
+                'addition' => [
+                    'show_total' => false,
+                    'total_text' => 'Deliveries',
+                    'total_button' => true,
+                    'format_amount' => true,
+                    'amount_prefix' => '£',
+                    'add_button_text' => 'Add Delivery',
+                    'individual_name' => 'Delivery',
+                    'form_name' => 'Add Delivery',
+                    'additional_fields' => ['Date' => '', 'Notes' => ''],
+                ],
+                'action' => [
+                    'type' => 'api',
+                    'set' => 'content.data.$[content_name=Delivery Information].content.Totals Multi',
+                ],
+                'api' => [
+                    'method' => 'put',
+                    'path' => 'orders/${id}'
+                ]
+            ],
+            'Delivery Invoice' => [
+                'prop' => 'Delivery_invoice',
+                'sortable' => true,
+                'label' => 'Delivery Invoice',
+                'align' => 'left',
+                'resizable' => true,
+                'type' => 'button',
+                'empty_background' => $red,
+                'filled_background' => $green,
+                'width' => '130px',
+                'value' => 'content.data.$[content_name=Delivery Information].content.invoice',
+                'button' => [
+                    'type' => 'danger',
+                    'plain' => true,
+                    'size' => 'mini',
+                    'text' => 'Recieved',
+                    'show_value' => true,
+                    'value' => 'content.data.$[content_name=Delivery Information].content.invoice',
+                    'hide_if_value' => true
+                ],
+                'action' => [
+                    'type' => 'api',
+                    'set' => 'content.data.$[content_name=Delivery Information].content.invoice',
+                    'value' => now()->format('d-m-Y')
+                ],
+                'api' => [
+                    'method' => 'put',
+                    'path' => 'orders/${id}'
+                ]
+            ],
           ]
-
-      ],
-
-      'col_colours' => [
-          'orders' => [
-              [
-                  'name' => 'Delivery',
-                  'if' => 'status',
-                  'value' => App\Order::$statuses['STATUS_PROCESSING'],
-                  'colour' => $blue
-              ],
-              [
-                  'name' => 'Delivery',
-                  'if' => 'status',
-                  'value' => App\Order::$statuses['STATUS_COMPLETED'],
-                  'colour' => $green
-              ],
-          ]
-      ],
+        ],
+        'col_colours' => [
+            'orders' => [
+                [
+                    'name' => 'Delivery',
+                    'if' => 'status',
+                    'value' => App\Order::$statuses['STATUS_PROCESSING'],
+                    'colour' => $blue
+                ],
+                [
+                    'name' => 'Delivery',
+                    'if' => 'status',
+                    'value' => App\Order::$statuses['STATUS_COMPLETED'],
+                    'colour' => $green
+                ]
+            ]
+        ],
   ],
 
   'default_content' => [
-      'orders' => [
-          ['content_name' => 'Delivery Information', 'content' => ['Totals Multi' => [], 'invoice' => ''], 'type' => 'json', 'order' => 1],
-          ['content_name' => 'In Production', 'content' => ['date' => '', 'notes' => ''], 'type' => 'json', 'order' => 2],
-          ['content_name' => 'Delivery Date', 'content' => ['date' => ''], 'type' => 'json', 'order' => 3],
-          ['content_name' => 'Spec Completed', 'content' => ['date' => ''], 'type' => 'json', 'order' => 4],
-          ['content_name' => 'Materials Ordered', 'content' => ['date' => ''], 'type' => 'json', 'order' => 5],
-          ['content_name' => 'Notes', 'content' => ['materials' => '', 'delivery' => '', 'completed checkbox' => false], 'type' => 'json', 'order' => 6],
-      ],
+
+      'orders' => [],
+
       'products' => [
           ['content_name' => 'Main Content', 'content' => '', 'type' => 'quill'],
           ['content_name' => 'Snippet', 'content' => '', 'type' => 'text']
       ],
-      'collection_types' => [
+
+      'pages' => [
           ['content_name' => 'Main Content', 'content' => '', 'type' => 'quill'],
+          ['content_name' => 'Snippet', 'content' => '', 'type' => 'text']
       ],
   ],
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ProductSeeder::class);
-        // $this->call(RolesAndPermissionsSeeder::class);
+        $this->call(RolesAndPermissionsSeeder::class);
     }
 }
